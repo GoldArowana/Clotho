cc26e3c7ad272a576be31fb3b644f4c371d73121
arowana <arowana.java@qq.com>
2019-06-20 01:00:28 +0800

    :sparkles: 增加权限管理

--- clotho-dal/src/main/java/com/aries/auth/clotho/dal/repository/UserRoleRepository.java
+++ clotho-dal/src/main/java/com/aries/auth/clotho/dal/repository/UserRoleRepository.java
@@ -1,20 +1,37 @@
 package com.aries.auth.clotho.dal.repository;
 
+import com.aries.auth.clotho.dal.mapper.AuthorityMapper;
 import com.aries.auth.clotho.dal.mapper.UserRoleMapper;
+import com.aries.auth.clotho.dal.po.Authority;
 import com.aries.auth.clotho.dal.util.SqlSessionUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.session.SqlSession;
+import tk.mybatis.mapper.entity.Example;
 
 @Slf4j
 public class UserRoleRepository {
-    public static Boolean hasPermission(String database, long userId, String url) {
-//        if (StringUtils.isBlank(url)) {
-//            log.warn("参数异常, url is blank");
-//            return false;
-//        }
+    public static boolean hasPermission(String database, long userId, String url) {
         try (SqlSession session = SqlSessionUtil.openSession(database)) {
             UserRoleMapper roleMapper = session.getMapper(UserRoleMapper.class);
             return roleMapper.hasPermission(userId, url);
+        }
+    }
+
+    public static boolean isLimited(String database, String url) {
+        try (SqlSession session = SqlSessionUtil.openSession(database)) {
+            AuthorityMapper authorityMapper = session.getMapper(AuthorityMapper.class);
+            Example example = new Example(Authority.class);
+            example.createCriteria().andEqualTo("url", url);
+            return authorityMapper.selectCountByExample(example) > 0;
+        }
+    }
+
+    public static long authUrlCount(String database, String url) {
+        try (SqlSession session = SqlSessionUtil.openSession(database)) {
+            AuthorityMapper authorityMapper = session.getMapper(AuthorityMapper.class);
+            Example example = new Example(Authority.class);
+            example.createCriteria().andEqualTo("url", url);
+            return authorityMapper.selectCountByExample(example);
         }
     }
 }
--- clotho-server-thrift/src/main/java/com/aries/auth/clotho/server/thrift/impl/PermissionServerImpl.java
+++ clotho-server-thrift/src/main/java/com/aries/auth/clotho/server/thrift/impl/PermissionServerImpl.java
@@ -25,7 +25,20 @@ public class PermissionServerImpl implements PermissionServer.Iface {
             return false;
         }
         try {
-            return UserRoleRepository.hasPermission(companyHelper.getDatabaseName(), requestDTO.getUserId(), requestDTO.getUrl());
+            // 数据库中是否有user和url的关系
+            boolean hasPermission = UserRoleRepository.hasPermission(companyHelper.getDatabaseName(), requestDTO.getUserId(), requestDTO.getUrl());
+            // 如果有，那么说明该user有url的权限。如果没有，不一定是没有权限，也可能是该url不限制权限，任何人都可以访问。
+            if (hasPermission) {
+                return true;
+            }
+
+            // 判断url是否被限制，如果没被限制，那么返回true，并记录到布隆过滤器中，防止后续再次访问。
+            boolean limited = UserRoleRepository.isLimited(companyHelper.getDatabaseName(), requestDTO.getUrl());
+            if (!limited) {
+                return true;
+            }
+
+            return false;
         } catch (Exception e) {
             log.error("check permission error:{}", e.getMessage(), e);
             return false;
