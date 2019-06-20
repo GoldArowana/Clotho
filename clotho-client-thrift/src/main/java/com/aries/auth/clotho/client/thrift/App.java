ca520307d2727fac9098198d83784844c7e33200
arowana <arowana.java@qq.com>
2019-06-20 19:59:51 +0800

    :fire:

--- clotho-client-thrift/src/main/java/com/aries/auth/clotho/client/thrift/App.java
+++ clotho-client-thrift/src/main/java/com/aries/auth/clotho/client/thrift/App.java
@@ -10,6 +10,6 @@ import org.apache.thrift.transport.TTransportException;
 public class App {
     public static void main(String[] args) throws TTransportException {
 //        Boolean result = ThriftHelper.call("Clotho", PermissionServer.Client.class, client -> client.hasPermission(new CompanyDTO("aries", "123123", ""), new RequestDTO(25, "/")));
-        System.out.println(PermissionFacade.hasPermission(RequestVO.RequestVOBuilder.aRequestVO().userId(25).url("/bbs").build()));
+        System.out.println(PermissionFacade.hasPermission(RequestVO.RequestVOBuilder.aRequestVO().userId(24).url("/bbs").build()));
     }
 }
